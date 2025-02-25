5dce1d96a4188ee1592af3f41598e5f946ae9249
kyfhy <lisatthu35@users.noreply.huggingface.co>
2025-02-25 15:10:57 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ which google-chrome
 
 #ls -l /
 
-google-chrome-stable --headless --no-sandbox --disable-dev-shm-usage --dump-dom https://www.google.com --disable-gpu --remote-debugging-port=9222 &
+#google-chrome-stable --headless --no-sandbox --disable-dev-shm-usage --dump-dom https://www.google.com --disable-gpu --remote-debugging-port=9222 &
 
 # Start the Java application 
 exec java -Dfile.encoding=UTF-8 -jar /target/dependency/webapp-runner.jar --port 7860 /target/*.war
