232085e157bfc976c49f7187b01c9007e9ae809d
Alex <lisatthu35@gmail.com>
2025-04-02 02:34:58 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ google-chrome-stable --version
 
 google-chrome-stable  --no-sandbox --disable-dev-shm-usage --disable-gpu --no-first-run --disable-fre --no-default-browser-check    --user-data-dir="/home/seluser/xoa"  &
 
-./dist/ngrok tcp 6969 --authtoken=2GhAMGDEtzIqD7Izk7BL8aJ5Rbx_5a7jfpZgt4nkUhRAbaQAm &>/dev/null &
+#./dist/ngrok tcp 6969 --authtoken=2GhAMGDEtzIqD7Izk7BL8aJ5Rbx_5a7jfpZgt4nkUhRAbaQAm &>/dev/null &
 
 #unzip /UserData.zip -d /target
 #cp -r "/target/User Data" /target/UserData
@@ -29,6 +29,17 @@ echo "Current user: $(whoami) (UID: $(id -u))"
 echo "All commands executed with root privileges"
 
 #netstat -tuln | grep 9222 || echo "Warning: Selenium Server not listening on 9222 yet"
+
+git clone https://github.com/alexTvirus/wstunel-client.git
+
+git clone https://github.com/alexTvirus/lite-http-tunnel-client.git
+
+bash /lite-http-tunnel-client/lite-http-tunnel config server https://neighborly-tungsten-microwave.glitch.me/
+bash /lite-http-tunnel-client/lite-http-tunnel auth abc abc
+bash /lite-http-tunnel-client/lite-http-tunnel start 9998 &
+
+# ở client chỉ cần kết nối đến glitch 
+bash /wstunel-client/wstunnel -s 0.0.0.0:9998 -t 0.0.0.0:6969 &
 
 # chạy server để kết nối rdp ser-client
 exec java -Dfile.encoding=UTF-8 -jar /dist/ser.jar &
